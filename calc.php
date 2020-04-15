0e7191a29502229bc23006d7c6f1da2caaee5ed0
yuusiki54 <yuusiki54@icloud.com>
2020-04-15 17:02:26 +0900

    色々整理

---
+++ calc.php
@@ -0,0 +1,64 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>計算機</title>
+</head>
+<body>
+    <h1>簡単な計算機</h1>
+    <form method=POST>
+        <input type="text" name="num1">
+       <select name="kigou" size="1">
+           <option value="plus">+</option>
+           <option value="minus">-</option>
+           <option value="mul">×</option>
+           <option value="div">÷</option>
+       </select>
+       <input type="text" name="num2">
+       <input type="submit" value="=">
+
+    </form>
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <!-- <form method="POST">
+        <input type="text" name="num1">
+        <select name="kigou">
+            <option value="plus">+</option>
+            <option value="minus">-</option>
+            <option value="mul">*</option>
+            <option value="div">/</option>
+        </select>
+        <input type="text" name="num2">
+        <input type="submit" value="=">
+    </form> -->
+</body>
+
+</body>
+</html>
