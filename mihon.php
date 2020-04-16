c852f89b04e09896d77905b89c3e364376a6e7d8
yuusiki54 <yuusiki54@icloud.com>
2020-04-16 21:00:10 +0900

    様々な変更、更新

---
+++ mihon.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1 class="font-weight-normal">PHP</h1>
+    </header>
+
+    <main>
+        <h2>Practice</h2>
+        <pre>
+            <?php
+            // ここにプログラムを記述します
+            ?>
+        </pre>
+    </main>
+
+</body>
+
+</html>
